aa25e46f42de0f34186ee586773a43c99a28aa1c
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-12 20:46:17 +0545

    initial skeleton code for the authorization in handlers ( jwt authentitcation used)

---
+++ middleware/auth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		ctx := context.WithValue(r.Context(), "userID", claims["user_id"])
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}

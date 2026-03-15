771613e19d1213c3f1059354be8fb7224fb309a2
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-15 21:33:25 +0545

    decoding and vlaidating the credentials that were passed as the json struct

--- handlers/auth.go
+++ handlers/auth.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
 )
 
 type AuthHandler struct {
@@ -10,4 +12,23 @@ type AuthHandler struct {
 
 func NewAuthHandler(db *sql.DB) *AuthHandler {
 	return &AuthHandler{DB: db}
+}
+
+func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, `{"error":""username,email and password are required}`, http.StatusBadRequest)
+		return
+	}
+
+	if len(body.Password) < 6 {
+		http.Error(w, `{"error":"password must be at least 6 characters"}`, http.StatusBadRequest)
+		return
+	}
+
 }

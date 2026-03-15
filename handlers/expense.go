cfe51a27d324a9c7e18e418ad6ee379c35db16c2
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-15 21:13:48 +0545

    added a template for the methods ( actions ) for the function handlers, for now

--- handlers/expense.go
+++ handlers/expense.go
@@ -2,12 +2,33 @@ package handlers
 
 import (
 	"database/sql"
+	"net/http"
 )
 
 type ExpenseHandler struct {
 	DB *sql.DB
 }
 
-func NewExpenseHandler(db *sql) *ExpenseHandler {
+func NewExpenseHandler(db *sql.DB) *ExpenseHandler {
 	return &ExpenseHandler{DB: db}
+}
+
+func (h *ExpenseHandler) CreateExpense(w http.ResponseWriter, r *http.Request) {
+	//to be filled , lets not do this for now
+}
+
+func (h *ExpenseHandler) GetExpense(w http.ResponseWriter, r *http.Request) {
+	//to be filled , lets not do this for now
+}
+
+func (h *ExpenseHandler) GetExpenseById(w http.ResponseWriter, r *http.Request) {
+	//to be filled , lets not do this for now
+}
+
+func (h *ExpenseHandler) UpdateExpense(w http.ResponseWriter, r *http.Request) {
+	//to be filled , lets not do this for now
+}
+
+func (h *ExpenseHandler) DeleteExpense(w http.ResponseWriter, r *http.Request) {
+	//to be filled , lets not do this for now
 }

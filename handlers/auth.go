1b698a3a4e04fda958344b36921943b62ee727a6
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-16 20:22:17 +0545

    refactored the authorization part to be more secure and strict about cases

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,9 +1,17 @@
 package handlers
 
 import (
-	"database/sql"
-	"encoding/json"
-	"net/http"
+
+	  "database/sql"
+    "encoding/json"
+    "log"
+    "net/http"
+    "os"
+    "time"
+
+    "github.com/golang-jwt/jwt/v5"
+    "golang.org/x/crypto/bcrypt"
+    "ExpenseTracker/models"
 )
 
 type AuthHandler struct {
@@ -22,13 +30,27 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		http.Error(w, `{"error":""username,email and password are required}`, http.StatusBadRequest)
+		http.Error(w, `{"error":"invalid request body"}`, http.StatusBadRequest)
+		return
+	}
+
+	if body.Username == "" || body.Email == "" || body.Password == "" {
+		http.Error(w, `{"error":"username, email and password are required"}`, http.StatusBadRequest)
 		return
 	}
 
 	if len(body.Password) < 6 {
 		http.Error(w, `{"error":"password must be at least 6 characters"}`, http.StatusBadRequest)
 		return
+	}
+
+	hashed, err :=bcrypt.GenerateFromPassword([]byte(body.Password),12)
+
+	if err !=nil{
+		log.Printf("Failed to create the password :%v",err)
+
+		 http.Error(w, `{"error":"internal server error"}`, http.StatusInternalServerError)
+        return
 	}
 
 }
